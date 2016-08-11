ed8c9466072abbdc6b149efebfe8466ae2dd29b3
triss14 <trishsalva@gmail.com>
2016-08-11 12:13:27 +0800

    Create SuccessMSG.java

---
+++ SuccessMSG.java
@@ -0,0 +1,42 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+public class SuccessMSG extends JFrame implements ActionListener{
+    
+    private EnrollmentSystem es;
+    private JLabel error;
+    private JButton ok;
+    
+    public ErrorMSG(EnrollmentSystem es){
+        JFrame p = new JFrame();
+        JOptionPane.showMessageDialog(p,
+        "Success!!",
+        "Machine Project",
+        JOptionPane.INFORMATION_MESSAGE);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.ok = new JButton("OK");
+        ok.addActionListener(this);
+    }
+    /*
+    public void initScreen(){
+        JPanel p = new JPanel(new GridBagLayout());
+        GridBagConstraints con = new GridBagConstraints();
+        con.insets = new Insets(10, 10, 10, 10);
+        
+        con.gridx = 0;
+        con.gridy = 0;
+        p.add(error, con);
+        con.gridx = 1;
+        p.add(ok, con);
+        
+        add(p);
+    }*/
+    
+    public void actionPerformed(ActionEvent e){
+        JButton b;
+        if(e.getActionCommand().equals("OK")){
+            this.dispose();
+        }
+    }
+    
+}
